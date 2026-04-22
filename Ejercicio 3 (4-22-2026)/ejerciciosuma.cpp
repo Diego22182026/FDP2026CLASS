634c23d338204cf45125896082a4859c3299ef65
Diego22182026 <00242126@uca.edu.sv>
2026-04-22 15:12:51 -0600

    Agregando ejercicio 3 al repositorio FDP2026CLASS

---
+++ Ejercicio 3 (4-22-2026)/ejerciciosuma.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int a,b;
+
+    cout <<"Por favor, ingrese el primer numero" << endl ;
+    cin >> a;
+
+    cout << "Por favor, ingrese el segundo numero" << endl;
+    cin >> b;
+
+    // Operaciones //
+
+    int suma = a+b;
+    
+    //Resultados//
+
+    cout << "Su resultado es: " << suma ;
+
+    return 0;
+}
